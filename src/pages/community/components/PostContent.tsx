09a36a4314aa584a012ffb848705cdb6bd3e77b8
fakeou <1596136105@qq.com>
2024-03-31 15:18:14 +0800

    fix: 隐藏tab

--- src/pages/community/components/PostContent.tsx
+++ src/pages/community/components/PostContent.tsx
@@ -1,23 +1,23 @@
 import { useEffect, useState } from "react";
 import SendPost from '../components/SendPost.tsx'
 import TWeetHome from '../../../components/tweetHome.js'
-import classNames from "classnames";
-interface TabType {
+/* import classNames from "classnames"; */
+/* interface TabType {
   label: 'For you' | 'Following',
   key: '1' | '2',
-}
+} */
 function CommunityContent({ name }: any) {
-  const [activeTab, setActiveTab] = useState<TabType['key']>('1');
+  /*   const [activeTab, setActiveTab] = useState<TabType['key']>('1'); */
   const [status, setStatus] = useState<any>(false);
 
-  const postTab: TabType[] = [{
-    label: 'For you',
-    key: '1'
-  },
-  {
-    label: 'Following',
-    key: '2'
-  }];
+  /*  const postTab: TabType[] = [{
+     label: 'For you',
+     key: '1'
+   },
+   {
+     label: 'Following',
+     key: '2'
+   }]; */
   const changeRefresh = (name: boolean) => {
     setStatus(name)
   }
@@ -31,7 +31,7 @@ function CommunityContent({ name }: any) {
   }, [])
   return (
     <div className="community-content">
-      {
+      {/* {
         name === 'dappCenter' ? '' : <div className="community-content-post-tab">
           {
             postTab.map((tab: TabType) => <div
@@ -39,11 +39,11 @@ function CommunityContent({ name }: any) {
               onClick={() => setActiveTab(tab.key)}><span>{tab.label}</span></div>)
           }
         </div>
-      }
+      } */}
       <div id="community-content-scroll" className="community-content-post"
         style={{
           overflowY: 'auto',
-          height: "calc(100vh - 129px)"
+          height: "calc(100vh - 79px)"
         }}>
         <SendPost changeRefresh={changeRefresh} />
         <TWeetHome scrollId='community-content-scroll' hei={'auto'} changeHei={() => { }} refresh={status} changeRefresh={changeRefresh} style={{ overflowY: 'none' }} />
