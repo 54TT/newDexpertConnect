473470a5e61d09a85d240e6e13d15754cd0a1a39
tt <1852918835@qq.com>
2024-04-03 23:32:21 +0800

    asr

--- src/pages/dapp/components/center.tsx
+++ src/pages/dapp/components/center.tsx
@@ -12,7 +12,7 @@ function Center() {
                     <img src="/bot.svg" alt="" loading={'lazy'}/>
                     <div className={'centerTopRight'} style={{paddingRight: browser ? '10%' : '2%'}}>
                         <p style={{fontSize: browser ? '30px' : '22px'}}>Token Creation Bot (Thor)</p>
-                        <p>{router.pathname === '/app' && router.search ? 'Thor is a user-friendly DApp that automates the ERC-20 token creation process. By providing an intuitive interface and simplifying complex tasks, Thor empowers users to efficiently create and deploy their tokens.' : 'In the Uniswap protocol design, the development team does not extract fees from transactions, and all fees in transactions are returned to the liquidity provider.'}</p>
+                        <p>{router.pathname === '/app'  ? 'Thor is a user-friendly DApp that automates the ERC-20 token creation process. By providing an intuitive interface and simplifying complex tasks, Thor empowers users to efficiently create and deploy their tokens.' : 'In the Uniswap protocol design, the development team does not extract fees from transactions, and all fees in transactions are returned to the liquidity provider.'}</p>
                         <div className={'dis'}>
                             {
                                 ['Video Guide', 'Start on Telegram', 'Start on Web'].map((i: string, ind: number) => {
