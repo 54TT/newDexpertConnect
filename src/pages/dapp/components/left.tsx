4d43698596dfff0234231543d5426eda94d1a1c0
tt <1852918835@qq.com>
2024-04-03 23:14:39 +0800

    all

--- src/pages/dapp/components/left.tsx
+++ src/pages/dapp/components/left.tsx
@@ -1,5 +1,4 @@
 import {useState} from "react";
-
 function Left() {
     const [value, setValue] = useState('Token Creation Bot')
     return (
@@ -14,21 +13,36 @@ function Left() {
                             i.map((item: string, it: number) => {
                                 return <p key={it} className={'list'} onClick={() => {
                                     if (value !== item) {
+                                        // {
+                                        //     if (it !== 2 && it !== 3 && it !== 1) {
+                                        //         setValue(item)
+                                        //     }
+                                        // } else
                                         if (ind === 0) {
-                                            if (it !== 2 && it !== 3) {
-                                                setValue(item)
-                                            }
-                                        } else {
                                             if (it !== 2 && it !== 1) {
                                                 setValue(item)
                                             }
                                         }
                                     }
                                 }}
-                                          style={{color: ind === 0 ? value === item ? 'rgb(134,240,151)' : it === 2 || it === 3 ? 'rgb(104,124,105)' : 'rgb(214, 223, 215)' : value === item ? 'rgb(134,240,151)' : it === 0 ? 'rgb(214, 223, 215)' : 'rgb(104,124,105)'}}>
+                                    // style={{color: ind === 0 ? value === item ? 'rgb(134,240,151)' : it === 2 || it === 1 || it === 3 ? 'rgb(104,124,105)' : 'rgb(214, 223, 215)' : value === item ? 'rgb(134,240,151)' : it === 0 ? 'rgb(214, 223, 215)' : 'rgb(104,124,105)'}}>
+                                          style={{color: item === 'Token Creation Bot' ? 'rgb(134,240,151)' : 'rgb(104,124,105)'}}>
                                     <img loading={'lazy'}
-                                        src={ind === 0 ? it === 0 ? "/token.svg" : it === 1 ? '/sniper.svg' : it === 2 ? '/dropBot.svg' : '/money.svg' : it === 0 ? '/news.svg' : it === 1 ? '/checker.svg' : '/trending.svg'}
-                                        alt=""/><span>{item}</span></p>
+                                         src={ind === 0 ? it === 0 ? "/token.svg" : it === 1 ? '/sniper.svg' : it === 2 ? '/dropBot.svg' : '/money.svg' : it === 0 ? '/news.svg' : it === 1 ? '/checker.svg' : '/trending.svg'}
+                                         alt=""/><span>{item}</span>
+                                    {
+                                        (item === 'Token Checker' || item === 'New Buy notification' || item === 'Trending' || item === 'Sniper Bot' || item === 'Air drop Bot' || item === 'Market maker') &&
+                                        <span style={{
+                                            fontSize: '10px',
+                                            marginLeft: '5px',
+                                            backgroundColor: 'rgb(40,40,40)',
+                                            padding: '4px',
+                                            display: 'block',
+                                            borderRadius: '6px',
+                                            whiteSpace:'nowrap'
+                                        }}>Coming soon</span>
+                                    }
+                                </p>
                             })
                         }
                     </div>
--- src/pages/dapp/index.tsx
+++ src/pages/dapp/index.tsx
@@ -2,9 +2,8 @@ import './index.less'
 import Left from './components/left.tsx'
 import Center from './components/center.tsx'
 import CommunityRight from "../community/components/RightSider.tsx";
-import {useContext} from "react";
+import {useContext,} from "react";
 import {CountContext} from "../../Layout.tsx";
-
 function Index() {
     const {browser}: any = useContext(CountContext);
     return (
