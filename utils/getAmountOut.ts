ba35d80813d2964d72e1415d6d81e2d7d4574c6b
fakeou <1596136105@qq.com>
2024-06-06 20:29:58 +0800

    feat: 调试方法

--- utils/getAmountOut.ts
+++ utils/getAmountOut.ts
@@ -1,53 +1,67 @@
 import Decimal from 'decimal.js';
-import {config} from '../src/config/config'
-import {getERC20Contract} from './contracts'
+import { config } from '../src/config/config';
+import { getERC20Contract } from './contracts';
 
 export const getAmountOut = async (
-    chainId: string,
-    universalRouterContract: any,
-    uniswapV2RouterContract: any,
-    tokenInAddress: string, 
-    tokenOutAddress: string,
-    inAmount: bigint, 
-    slippage: number,
-    payType: number
+  chainId: string,
+  universalRouterContract: any,
+  uniswapV2RouterContract: any,
+  tokenInAddress: string,
+  tokenOutAddress: string,
+  outAmount: bigint,
+  slippage: number,
+  payType: number
 ) => {
-    const chainConfig = config[chainId];
-    const ethAddress = chainConfig.ethAddress;
-    const wethAddress = chainConfig.wethAddress;
-    
-    let fee = 0;
+  const chainConfig = config[chainId];
+  const ethAddress = chainConfig.ethAddress;
+  const wethAddress = chainConfig.wethAddress;
 
-    if(payType == 0){
-        const fastTradeFeeBps = await universalRouterContract.fastTradeFeeBps();
-        const feeBaseBps = await universalRouterContract.feeBaseBps();
+  let fee = 0;
+  if (payType == 0) {
+    const fastTradeFeeBps = await universalRouterContract.fastTradeFeeBps();
+    const feeBaseBps = await universalRouterContract.feeBaseBps();
 
-        fee = fastTradeFeeBps / feeBaseBps
-    }
+    fee = fastTradeFeeBps / feeBaseBps;
+  }
 
-    let amountOut: Decimal = new Decimal(0)
+  let amountIn: Decimal = new Decimal(0);
 
-    if (ethAddress.toLowerCase() === tokenInAddress.toLowerCase() && wethAddress.toLowerCase() !== tokenOutAddress.toLowerCase()) {
-        const swapPath = [wethAddress, tokenOutAddress];
-        let amountsOut = await uniswapV2RouterContract.getAmountsOut(inAmount, swapPath)
-        amountOut = new Decimal(amountsOut[amountsOut.length - 1])
-    } else if (tokenInAddress.toLowerCase() !== wethAddress.toLowerCase() && tokenOutAddress.toLowerCase() === ethAddress.toLowerCase()) {
-        const swapPath = [tokenInAddress, wethAddress];
-        let amountsOut = await uniswapV2RouterContract.getAmountsOut(inAmount, swapPath)
-        amountOut = new Decimal(amountsOut[amountsOut.length - 1])
-    } else if (ethAddress.toLowerCase() !== tokenInAddress.toLowerCase() && ethAddress.toLowerCase() !== tokenOutAddress.toLowerCase() && wethAddress.toLowerCase() !== tokenInAddress.toLowerCase() && wethAddress.toLowerCase() !== tokenOutAddress.toLowerCase()) {
-        const swapPath = [tokenInAddress, wethAddress, tokenOutAddress];
-        let amountsOut = await uniswapV2RouterContract.getAmountsOut(inAmount, swapPath)
-        amountOut = new Decimal(amountsOut[amountsOut.length - 1])
-    } else {
-        amountOut = new Decimal(inAmount.toString());
-    }
+  if (
+    ethAddress.toLowerCase() === tokenInAddress.toLowerCase() &&
+    wethAddress.toLowerCase() !== tokenOutAddress.toLowerCase()
+  ) {
+    const swapPath = [wethAddress, tokenOutAddress];
+    amountIn = new Decimal(
+      (await uniswapV2RouterContract.getAmountsIn(outAmount, swapPath))[0]
+    );
+  } else if (
+    tokenInAddress.toLowerCase() !== wethAddress.toLowerCase() &&
+    tokenOutAddress.toLowerCase() === ethAddress.toLowerCase()
+  ) {
+    const swapPath = [tokenInAddress, wethAddress];
+    amountIn = new Decimal(
+      (await uniswapV2RouterContract.getAmountsIn(outAmount, swapPath))[0]
+    );
+  } else if (
+    ethAddress.toLowerCase() !== tokenInAddress.toLowerCase() &&
+    ethAddress.toLowerCase() !== tokenOutAddress.toLowerCase() &&
+    wethAddress.toLowerCase() !== tokenInAddress.toLowerCase() &&
+    wethAddress.toLowerCase() !== tokenOutAddress.toLowerCase()
+  ) {
+    const swapPath = [tokenInAddress, wethAddress, tokenOutAddress];
+    let amountsOut = await uniswapV2RouterContract.getAmountsOut(
+      outAmount,
+      swapPath
+    );
+  } else {
+    amountIn = new Decimal(outAmount.toString());
+  }
 
-    if (fee > 0){
-        amountOut = amountOut.add(amountOut.mul(fee))
-    }
-    if (slippage > 0){
-        amountOut = amountOut.add(amountOut.mul(slippage))
-    }
-    return amountOut
-}
+  if (fee > 0) {
+    amountIn = amountIn.sub(amountIn.mul(fee));
+  }
+  if (slippage > 0) {
+    amountIn = amountIn.sub(amountIn.mul(slippage));
+  }
+  return amountIn;
+};
