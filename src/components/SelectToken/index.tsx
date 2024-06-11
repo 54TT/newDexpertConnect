32f762a65aa0d76ebe6845767083525c1f1d58e1
fakeou <1596136105@qq.com>
2024-06-11 16:02:46 +0800

    feat: model完善

--- src/components/SelectToken/index.tsx
+++ src/components/SelectToken/index.tsx
@@ -17,14 +17,36 @@ interface SelectTokenType {
 
 const { Search } = Input;
 
-const mockTokenList: TokenItemData[] = new Array(5).fill(0).map(() => ({
-  symbol: 'ETH',
-  name: 'ETH',
-  address: '0x000000000000000000',
-  id: '0',
-  icon: '/eth1.svg',
-  balance: '1',
-}));
+const mockTokenETH = '0x0000000000000000000000000000000000000000';
+const mockTokenWETH = '0xfff9976782d46cc05630d1f6ebab18b2324d6b14';
+const mockTokenUSDT = '0xb72bc8971d5e595776592e8290be6f31937097c6';
+
+const mockTokenList: TokenItemData[] = [
+  {
+    symbol: 'ETH',
+    name: 'ETH',
+    address: mockTokenETH,
+    id: '0',
+    icon: '/eth1.svg',
+    balance: '0.1',
+  },
+  {
+    symbol: 'WETH',
+    name: 'WETH',
+    address: mockTokenWETH,
+    id: '1',
+    icon: '/powEth.svg',
+    balance: '0.02',
+  },
+  {
+    symbol: 'USDT',
+    name: 'USDT',
+    address: mockTokenUSDT,
+    id: '2',
+    icon: '/usdt.svg',
+    balance: '200',
+  },
+];
 function SelectToken({ onChange }: SelectTokenType) {
   const [tokenList, setTokenList] = useState<TokenItemData[]>(mockTokenList);
   const [searchList, setSearchList] = useState<TokenItemData[]>();
@@ -41,7 +63,7 @@ function SelectToken({ onChange }: SelectTokenType) {
       {tokenList?.map?.((item: TokenItemData) => (
         <div className="select-token-item" onClick={() => onChange(item)}>
           <div className="select-token-item-info">
-            <img src="/eth1.svg" alt="" />
+            <img src={item.icon} alt="" />
             <div>
               <span className="select-token-item-info-symbol">
                 {item.symbol}
