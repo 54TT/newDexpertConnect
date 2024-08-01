91cd38ce4dbf5ce9283c207ebf612a066634c504
fakeou <1596136105@qq.com>
2024-08-01 17:18:05 +0800

    fix: 修改bitlayer链

--- src/pages/dapps/swap/components/SwapComp/index.tsx
+++ src/pages/dapps/swap/components/SwapComp/index.tsx
@@ -543,7 +543,8 @@ function SwapComp({ initChainId, initToken, changeAble = true }: SwapCompType) {
       NotificationChange('error', t('Slider.err'));
       setButtonLoading(false);
       setButtonDescId('1');
-      return null;
+      console.error(e);
+      return;
     }
     setButtonLoading(false);
     setButtonDescId('1');
